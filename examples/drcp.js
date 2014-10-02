949bd35c507b8ef4964fa0b50cf6bc6b6674cd83
chemdemo <yangdemo@gmail.com>
2014-10-02 16:03:15 +0800

    add example code

---
+++ examples/drcp.js
@@ -0,0 +1,35 @@
+/*
+* @Author: dm.yang
+* @Date:   2014-10-02 15:39:40
+* @Last Modified by:   dm.yang
+* @Last Modified time: 2014-10-02 15:56:19
+*/
+
+var DRCP = require('stormdrcp-node');
+
+var client = DRCP({
+    host: '127.0.0.1',
+    port: 3772,
+    timeout: 1000,
+    keepalive: true,
+    maxConnectCounts: 30
+});
+
+client.on('error', function(err) {
+    throw err;
+});
+
+// promise way
+client.execute('topology-name', JSON.stringify(data))
+    .then(function(res) {
+        console.log(res);
+    })
+    .catch(function(err) {
+        throw err;
+    });
+
+// callback way
+client.execute('topology-name', JSON.stringify(data), function(err, res) {
+    if(err) throw err;
+    else console.log(res);
+});
