1fd0eee4cc82a11b4a6177ec8a7c06cd0ec93e98
chemdemo <yangdemo@gmail.com>
2014-10-02 17:02:20 +0800

    change file name to `DRPC`

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: dm.yang
 * @Date:   2014-10-02 15:32:01
 * @Last Modified by:   dm.yang
-* @Last Modified time: 2014-10-02 15:32:27
+* @Last Modified time: 2014-10-02 17:01:47
 */
 
-module.exports = require('./lib/DRCP');
+module.exports = require('./lib/DRPC');
